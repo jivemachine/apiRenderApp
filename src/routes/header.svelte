6ba009b59f7414e91d4d14e5daa4d6f25ded9756
Jeremy <the.jeremy.cobb@gmail.com>
2024-04-09 21:37:50 -0500

    add header & footer components

---
+++ src/routes/header.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+    // default config settings
+    import * as config from '$lib/config'
+</script>
+
+<!-- navbar -->
+<nav>
+    <!-- Title -->
+    <a href="/" class="title">
+        <b>{config.title}</b>
+    </a>
+
+    <!-- actual nav -->
+    <ul class="links">
+        <li>
+            <a href="/about">About</a>
+        </li>
+        <li>
+            <a href="/contact">Contact</a>
+        </li>
+        <li>
+            <a href="/blog">Start Learning</a>
+        </li>
+    </ul>
+
+    <!-- Theme -->
+    <button>Toggle</button>
+</nav>
