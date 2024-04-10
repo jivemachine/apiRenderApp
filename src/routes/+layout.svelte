4bd7ac55f7a9a0bc897934e55afe7c8352535da8
Jeremy <the.jeremy.cobb@gmail.com>
2024-04-09 21:38:27 -0500

    added layout for page

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,26 @@
+<!-- +layout.svelte -->
+<script lang="ts">
+    // header & footer components
+    import Footer from './footer.svelte'
+    import Header from './header.svelte'
+
+    // open props 
+    import 'open-props/style'
+    import 'open-props/normalize'
+    import 'open-props/buttons'
+
+    // import app.css
+    import '../app.css'
+</script>
+
+<div class="layout">
+    <!-- Header -->
+    <Header />
+
+    <main>
+        <slot />
+    </main>
+
+    <!-- Footer -->
+    <Footer />
+</div>
