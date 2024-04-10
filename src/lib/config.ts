f8535b8d9a661ecf12c70df8e382765c8de58772
Jeremy <the.jeremy.cobb@gmail.com>
2024-04-09 21:24:49 -0500

    added config.ts file to make updating easier

---
+++ src/lib/config.ts
@@ -0,0 +1,6 @@
+import { dev } from '$app/environment';
+
+export const title = "Dsamajig";
+export const description = "Data Science Curriculum for the Rest of Us";
+export const url = dev ? 'http://localhost:5173' : 'https://dsamajig.com';
+
