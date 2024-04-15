ef42e5cca8a7a069bf029b541ab470c59ee62a47
Jeremy <the.jeremy.cobb@gmail.com>
2024-04-15 12:35:01 -0500

    add data types for markdown posts

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,10 +1,11 @@
+// $lib/types.ts
 export type Categories = 'sveltekit' | 'svelte'
 
 export type Post = {
-    title: string
-    slug: string
-    description: string
-    date: string
-    categories: Categories[]
-    published: boolean
+	title: string
+	slug: string
+	description: string
+	date: string
+	categories: Categories[]
+	published: boolean
 }
