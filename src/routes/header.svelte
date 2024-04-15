f9b0b17040ae805449a878655db1edcf2f1223f4
Jeremy <the.jeremy.cobb@gmail.com>
2024-04-15 11:19:23 -0500

    added styling for different pages

--- src/routes/header.svelte
+++ src/routes/header.svelte
@@ -26,3 +26,31 @@
     <!-- Theme -->
     <button>Toggle</button>
 </nav>
+
+<style>
+	nav {
+		padding-block: var(--size-7);
+	}
+
+	.links {
+		margin-block: var(--size-7);
+	}
+
+	a {
+		color: inherit;
+		text-decoration: none;
+	}
+
+	@media (min-width: 768px) {
+		nav {
+			display: flex;
+			justify-content: space-between;
+		}
+
+		.links {
+			display: flex;
+			gap: var(--size-7);
+			margin-block: 0;
+		}
+	}
+</style>
